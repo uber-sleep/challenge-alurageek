726df4aaaadf4bf1b52b85b9595d36538d31db46
carolgs <goncalves.macarolina@gmail.com>
2024-07-01 18:16:51 -0300

    feat: funções de get e post da api

---
+++ js/connect.js
@@ -0,0 +1,49 @@
+const endpoint = "http://localhost:3000/produtos";
+
+async function getApi() {
+    try {
+        const response = await fetch(endpoint);
+        
+        if(!response.ok) {
+            throw new Error(`Não foi possível obter os produtos.`);
+        };
+
+        const data = await response.json();
+        
+        return data;
+    } catch (e){
+        console.error(e);
+        throw e;
+    };
+};
+
+async function postToApi(image, alt, name, price, id ) {
+    try {
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                imagem: image,
+                alt: alt,
+                nome: name,
+                preco: price,
+                id: id
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error("Não foi possivel enviar o vídeo!")
+        };
+
+        const data = await response.json();
+        
+        return data;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    };
+};
+
+export const apiMethod = { getApi, postToApi };
